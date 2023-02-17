04dab0f8d99ab7df4f64a6fb69b2d238192247ab
EkaFahrikaNariswari <ekapahrika23@gmail.com>
2023-02-17 22:37:09 +0700

    first

--- QE_Eka-Fahrika/src/Main.java
+++ QE_Eka-Fahrika/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world! Eka Fahrika heeee");
+        System.out.println("Hello world! Eka Fahrika");
     }
 
 }
